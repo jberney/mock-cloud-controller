9e6ba24aefd7de099ef053d413d0e91d25ca4016
Jonathan Berney <jberney1@icloud.com>
2016-10-16 11:20:57 -0700

    build ssl keys/cert dynamically

--- src/app_factory.js
+++ src/app_factory.js
@@ -6,12 +6,12 @@ const RouterFactory = require('./router_factory');
 module.exports = {
     newApp(state) {
         const app = express();
+        app.use(bodyParser.json());
         app.use(function (req, res, next) {
             console.log(`[CC] ${req.method} ${req.url}`);
             req.body && console.log(req.body);
             next();
         });
-        app.use(bodyParser.json());
         app.use('/v2', RouterFactory.newRouter(state));
         return app;
     }
--- src/server_factory.js
+++ src/server_factory.js
@@ -1,11 +1,42 @@
-const fs = require('fs');
 const https = require('https');
-const path = require('path');
+const {pki} = require('node-forge');
 
 const AppFactory = require('./app_factory');
 
-const key = fs.readFileSync(path.resolve(__dirname, '..', 'sslcert', 'key.pem'), 'utf8');
-const cert = fs.readFileSync(path.resolve(__dirname, '..', 'sslcert', 'cert.pem'), 'utf8');
+const attrs = [{
+    name: 'commonName',
+    value: 'localhost'
+}, {
+    name: 'countryName',
+    value: 'some-country'
+}, {
+    shortName: 'ST',
+    value: 'some-state'
+}, {
+    name: 'localityName',
+    value: 'some-locality'
+}, {
+    name: 'organizationName',
+    value: 'some-org'
+}, {
+    shortName: 'OU',
+    value: 'some-org-unit'
+}];
+
+const keys = pki.rsa.generateKeyPair(2048);
+const key = pki.privateKeyToPem(keys.privateKey);
+
+const certificate = pki.createCertificate();
+certificate.publicKey = keys.publicKey;
+certificate.serialNumber = '01';
+certificate.validity.notBefore = new Date();
+certificate.validity.notAfter = new Date();
+certificate.setSubject(attrs);
+certificate.setIssuer(attrs);
+certificate.sign(keys.privateKey);
+
+const cert = pki.certificateToPem(certificate);
+
 const credentials = {key, cert};
 
 module.exports = {
